7883b2e481d8f0a3bc8e65a3936d411c3fc2d5c4
sadiq-1698 <mohammedsadiq1698@gmail.com>
2020-10-31 13:12:26 +0530

    Banner UI done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,40 @@ import './App.css';
 function App() {
   return (
     <div className="main-wrapper">
-      <h1>Hello App</h1>
+      <Banner />
+    </div>
+  );
+}
+
+const Banner = () => {
+  return (
+    <div className="banner">
+        <img src={process.env.PUBLIC_URL + '/pattern-bg.png'} alt="top-banner" />
+        <h1 className="banner-header">IP Address Tracker</h1>
+        <div className="textfield-container">
+          <input type="text" placeholder="Search for any IP address or domain"/>
+          <button>
+            <img src={process.env.PUBLIC_URL + '/icon-arrow.svg'} alt="top-banner" />
+          </button>
+        </div>
+        <div className="location-container">
+            <div className="header-span">
+              <span>IP Address</span>
+              <h2>192.168.1.10</h2>
+            </div>
+            <div className="header-span">
+              <span>Location</span>
+              <h2>Brooklyn, NY, USA</h2>
+            </div>            
+            <div className="header-span">
+              <span>Timezone</span>
+              <h2>UTC : 05:00</h2>
+            </div>            
+            <div className="header-span">
+              <span>ISP</span>
+              <h2>SpaceX Starlink</h2>
+            </div>
+        </div>
     </div>
   );
 }
